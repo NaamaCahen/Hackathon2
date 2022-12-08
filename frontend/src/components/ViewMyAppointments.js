89fbdf65c62b74d900ff276eef1f6744d0ca3f62
NaamaCahen <cahenbb@gmail.com>
2022-12-08 15:59:31 +0200

    delete appointment button

--- frontend/src/components/ViewMyAppointments.js
+++ frontend/src/components/ViewMyAppointments.js
@@ -18,6 +18,30 @@ export class ViewMyAppointments extends Component {
             this.setState({appointments:data})
         })
     }
+    componentDidUpdate(){
+        fetch(`http://localhost:5001/myAppointments/${this.state.currentPatient.p_id}`)
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            this.setState({appointments:data})
+        })
+    }
+    handleEdit(){
+        
+    }
+    handleDelete(e){
+        const id=e.target.parentNode.id
+        fetch(`http://localhost:5001/myAppointments/${id}`,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            alert(`the appointment was deleted. \n Thank You!`)
+        })
+        .catch(e=>console.log(e))
+        
+    }
     render() {
         return (
             <>
@@ -25,12 +49,12 @@ export class ViewMyAppointments extends Component {
                     {
                         this.state.appointments.map((item,i)=>{
                             return(
-                               <div key={i}>
+                               <div key={i} id={item.appointment_id}>
                                 <h4>Dr. {item.dr_last_name}  {item.dr_first_name}</h4>
                                 <p>{item.appointment_time.split('T')[0]}</p>
                                 <p>{item.appointment_time.split('T')[1].slice(0,5)}</p>
-                                <button>edit</button>
-                                <button>delete</button>
+                                <button onClick={this.handleEdit}>edit</button>
+                                <button onClick={this.handleDelete}>delete</button>
                                </div> 
                             )
                         })
