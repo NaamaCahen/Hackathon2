9554d2e9b77ac86734df965da427d668c7c87522
NaamaCahen <cahenbb@gmail.com>
2022-12-08 19:06:58 +0200

    styling the welcome page

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,10 +1,16 @@
 import { Routes, Route, Link } from 'react-router-dom';
+import './Welcome.css'
 const Welcome = (props) => {
     return (
         <>
-            <h1 >Welcome to MY HEALTH CARE CLINIC! </h1>
-            <Link to='/login'>login</Link>
-            new here?<Link to='/register'>register</Link>
+        
+            <div className='tc flex flex-column items-center flex-wrap mb-50 centerDiv'>
+                <img src="https://myhealthcareclinic.com/wp-content/uploads/logo-1.png" alt='logo' width='70%' />
+                <p className='login'><Link to='/login'>login</Link> <span>new here? <Link to='/register'>register</Link> </span>
+                </p>
+                
+            </div>
+            
         </>
     )
 }
