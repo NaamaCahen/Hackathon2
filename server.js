2469d33c29a5d7bdf21f263325d987f175d156dd
Naama Partouche (Cahen) <113530743+NaamaCahen@users.noreply.github.com>
2023-07-23 12:34:05 +0300

    Update server.js
    deleting the last commit change

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const __dirname=path.resolve();
 app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.get('*',(req,res)=>{
